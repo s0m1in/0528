09fd5742e0224f81aa053b56703fed4fab7abe91
sam <sam@myla.gotdns.com>
2015-05-30 12:09:17 +0800

    finish create folder, s3fs_key, mount buckets

--- cookbooks/mount/recipes/default.rb
+++ cookbooks/mount/recipes/default.rb
@@ -9,14 +9,23 @@
 mount = data_bag('mount')
 mount.each do |id|
 	mo = data_bag_item('mount', id)
-#	aa(id) do
-#	name	['id']
-#	buckets	['buckets']
-#	access_key	['access_key_id']
-#	secret_access_key	['secret_access_key']
-#	end
-  directory "/mnt/#{id}" do
-    action :create
-    recursive true
+  user(id) do
+  end
+    directory "/mnt/#{id}" do
+      action :create
+      recursive true
+      not_if { File.directory?("/mnt/#{id}") }
+    end
+  s3fskey = mo['buckets'] + ":" + mo['access_key_id'] + ":" + mo['secret_access_key']
+  file "/etc/passwd-s3fs" do
+    mode 0600
+    content "#{s3fskey}"
+  end
+  bash 'mount buckets' do
+    mount_condition = "-o allow_other -o default_acl=public-read -o use_cache=/tmp"
+    bucket_name = mo['buckets']
+    code <<-EOF
+    s3fs "#{bucket_name}" "/mnt/#{id}" #{mount_condition}
+    EOF
   end
 end
